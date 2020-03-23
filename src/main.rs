fc6bd4cc901904405337b259dfc744eef4395e73
Joe Previte <jjprevite@gmail.com>
2020-03-22 20:41:31 -0700

    feat: add fns, get tests passing

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,12 @@ fn main() {
     println!("Hello, world!");
 }
 
-// fn to convert
-// fn to ask for number
-// lets start with a test
-
-fn convert_c_to_f(n: i32) -> i32 {
-    let x = n;
-    0
+fn convert_c_to_f(n: f64) -> f64 {
+    (n * 1.8) + 32.0
 }
 
-fn convert_f_to_c(n: i32) -> i32 {
-    let x = n;
-    0
+fn convert_f_to_c(n: f64) -> f64 {
+    (n - 32.0) / 0.5556
 }
 
 #[cfg(test)]
@@ -23,16 +17,16 @@ mod tests {
   #[test]
   fn test_convert_c_to_f() {
       // It should correctly convert a C temperature to F.
-      assert_eq!(convert_c_to_f(-40), -40);
-      assert_eq!(convert_c_to_f(0), -18);
-      assert_eq!(convert_c_to_f(32), 0);
+      assert_eq!(convert_c_to_f(-40.0), -40.0);
+      assert_eq!(convert_c_to_f(0.0), 32.0);
+      assert_eq!(convert_c_to_f(32.0), 89.6);
   }
 
   #[test]
   fn test_convert_f_to_c() {
       // It should correctly convert a C temperature to F.
-      assert_eq!(convert_c_to_f(-40), -40);
-      assert_eq!(convert_c_to_f(0), 32);
-      assert_eq!(convert_c_to_f(40), 104);
+      assert_eq!(convert_c_to_f(-40.0), -40.0);
+      assert_eq!(convert_c_to_f(0.0), 32.0);
+      assert_eq!(convert_c_to_f(40.0), 104.0);
   }
 }
