70c54e01d63590d30c9fff192da2c538bf0fbcb0
Joe Previte <jjprevite@gmail.com>
2020-03-22 20:02:11 -0700

    feat: add failing tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// fn to convert
+// fn to ask for number
+// lets start with a test
+
+fn convert_c_to_f(n: i32) -> i32 {
+    let x = n;
+    0
+}
+
+fn convert_f_to_c(n: i32) -> i32 {
+    let x = n;
+    0
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_convert_c_to_f() {
+      // It should correctly convert a C temperature to F.
+      assert_eq!(convert_c_to_f(-40), -40);
+      assert_eq!(convert_c_to_f(0), -18);
+      assert_eq!(convert_c_to_f(32), 0);
+  }
+
+  #[test]
+  fn test_convert_f_to_c() {
+      // It should correctly convert a C temperature to F.
+      assert_eq!(convert_c_to_f(-40), -40);
+      assert_eq!(convert_c_to_f(0), 32);
+      assert_eq!(convert_c_to_f(40), 104);
+  }
+}
