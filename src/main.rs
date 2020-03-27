f1b05481c024b2418652bae049ce78f2eb655439
Joe Previte <jjprevite@gmail.com>
2020-03-26 21:26:08 -0700

    feat: add funcs to ask for number

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,61 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n༼ つ ◕_◕ ༽つ The temperature converter! ☁︎");
+
+    // Use it to store the either C or F
+    let mut unit_type = String::new();
+
+    // Use to store the number to convert
+    let mut num_to_convert = String::new();
+
+    // Ask if user wants to convert C to F, or F to C
+    loop {
+        println!("\nPlease enter the unit you're converting from: C or F");
+
+
+        io::stdin().read_line(&mut unit_type)
+            .expect("Failed to read line");
+
+        // Trim to remove the line break added when the user hits "enter"
+        match unit_type.trim() {
+            "C" => println!("\nConverting from C to F"),
+            "F" => println!("\nConverting from F to C"),
+            _ => {
+                println!("\nOops...that doesn't look C or F");
+                continue;
+            },
+        };
+
+        break;
+    }
+
+    loop {
+        println!("\nPlease enter the number you want to convert:");
+
+        io::stdin().read_line(&mut num_to_convert)
+            .expect("Failed to read line");
+
+        let num_to_convert: u32 = match num_to_convert.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let converted = match unit_type.trim() {
+            "C" => convert_c_to_f(num_to_convert as f64),
+            "F" => convert_f_to_c(num_to_convert as f64),
+            _ => -1.0, // TODO fix this
+        };
+
+        let output_unit = match unit_type.trim() {
+            "C" => "F",
+            "F" => "C",
+            _ => "Unknown type"
+        };
+
+        println!("\n{}°{} is equal to {}°{}", num_to_convert, unit_type.trim(), converted.round(), output_unit);
+        break;
+    }
 }
 
 fn convert_c_to_f(n: f64) -> f64 {
